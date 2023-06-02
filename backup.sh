b1b0bc38da3d1defd46fdd9642567e5cb9ebef7a
Ailakks <hola@cortmnzz.com>
2023-06-03 00:51:33 +0200

    feat: private repos

--- backup.sh
+++ backup.sh
@@ -20,36 +20,3 @@ for folder in "$BASE_FOLDER"/*; do
     gh repo view "aservers/$LOWERCASE_NAME" &> /dev/null
     REPO_EXISTS=$?
 
-    if [ $REPO_EXISTS -eq 0 ]; then
-      echo "Repository already exists: aservers/$LOWERCASE_NAME"
-    else
-      # Add a hyphen if the name contains lowercase letters
-      if [[ "$REPO_NAME" != "$LOWERCASE_NAME" ]]; then
-        LOWERCASE_NAME="$LOWERCASE_NAME-"
-      fi
-
-      # Initialize Git repository
-      git init
-
-      # Add all files to the repository
-      git add .
-
-      # Commit the changes
-      git commit -m "initial commit"
-
-      # Create the remote repository
-      gh repo create "aservers/$LOWERCASE_NAME$REPO_NAME" --public --confirm
-
-      # Set the remote origin
-      git remote add origin "https://github.com/aservers/$LOWERCASE_NAME$REPO_NAME"
-
-      # Push to the remote repository
-      git push -u origin master
-
-      echo "Repository created: aservers/$LOWERCASE_NAME$REPO_NAME"
-    fi
-
-    # Go back to the base folder
-    cd "$BASE_FOLDER" || exit
-  fi
-done
